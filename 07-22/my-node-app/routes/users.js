3f632f64f1774786a1c9a95aee7179895a2af429
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-07-23 11:15:19 +0900

    07-22 complete

--- 07-22/my-node-app/routes/users.js
+++ 07-22/my-node-app/routes/users.js
@@ -1,8 +1,17 @@
+//users.js 라우터파일의 기본 호출주소체계는
+//app.js에서 정의한 http://localhost:3000/users
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+//http://localhost:3000/users/
 router.get('/', function(req, res, next) {
+  //해당 텍스트를 웹브라우저에 서버 응답결과물론 반환
+  res.send('respond with a resource');
+});
+
+//http://localhost:3000/users/testing
+router.get('/testing', function(req, res, next) {
   res.send('respond with a resource');
 });
 
