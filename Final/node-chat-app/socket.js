73d0763c6048ce85c9f4317bfd7787e464b5e5b5
YonghoBae <dyddyd134@chungbuk.ac.kr>
2024-07-31 15:45:24 +0900

    socket chat 추가

---
+++ Final/node-chat-app/socket.js
@@ -0,0 +1,66 @@
+//메시지 서버 소켓파일
+//해당 socket.js 모듈이 메시징 서버 역할을 제공
+
+//socket.io 팩키지 참조
+const SocketIO = require("socket.io");
+
+//socket.js모듈 기능정의
+module.exports =(server)=>{
+
+    //서버 소켓의 입출력(In/Out) 메시지 처리 객체 io 생성
+    //input 메시지는 웹브라우저에서 들어오는 메시지
+    //output 메시지는 서버소켓에서 웹브라우저로 전송하는 메시지
+    const io = SocketIO(server,{path:"/socket.io"});
+
+    //자바스킄립트에서 on(이벤트핸들러(처리기))
+    //io객체에 connection 이벤트가 발생하면 콜백함수를 실행
+    //connection 이벤트는 웹브라우저와 서버소켓과 연결이 완료되면 발생
+    io.on("connection",(socket)=>{
+
+        //socket은 현재 연결된 사용자(웹브라우저) 서버소켓간 연결 정보를 제공
+        //웹브라우저에서 서버소켓에 broadcast라는 이벤트 수신기를 호출하면 관련 콜백함수가 실행
+        //socket.on("서버소켓 이벤트 수신기명",처리할 콜백함수)
+
+        //웹브라우저(클라이언트/프론트엔드)에서 서버소켓에 broadcast라는 이벤트를 호출하면
+        //서버소켓에서 클라이언트에서 보내준 메시지를 수신하고 콜백함수를 통해
+        //서버에서 클라이언트로 메시지를 전송(io.emit())
+        socket.on("broadcast",function(msg){
+            //현재 메시지 서버에 연결된 모든 사용자들(웹브라우저/프론트엔드웹페이지)에게
+            //메시지를 전송하는데 클라이언트 메시지 수신 이벤트명이  receiveALl로 msg데이터를 전송
+            //io.emit()메소드는 서버소켓(io) 연결된 모든 사용자에게 메시지를 보낼때 사용..
+            //io.emit('클라이언트 이벤트수신기명',클라이언트로 보낼데이터');
+            io.emit("receiveAll",msg);
+        });
+
+        //지정한 채팅방 개설 및 입장처리 이벤트 수신기
+        socket.on("entry",async(channel,nickName)=>{
+            //socket.join('채팅방 이름');
+            //서버환경에 해당 채팅방이름으로 채널을 개설하고 현재 입장하는 사용자를 해당 채팅방 사용자로 등록처리
+            //이미 해당 채널이 개설되어 있으면 신규개설하지 않고 기존 채널로 입장
+            socket.join(channel);
+
+            //현재 접속자를 제외한 해당 채널에 이미 접속한 모든 사용자에게 메시지를 발송
+            //socket.to(channel).emit();
+            socket.to(channel).emit("entryOk",`${nickName}님이 ${channel} 채널에 입장했습니다.`);
+
+            //현재 채널에 입장하고 있는 사용자에게만 메시지 발송
+            //socket.emit(); 현재 서버소켓을 호출한(입장하는) 사용자에게만 메시지 발송
+            socket.emit("entryOk",`${nickName} 라는 대화명으로 ${channel} 채널에 입장했습니다.`);
+        });
+
+        //채팅방 기준으로 해당 채팅방의 나를 포함한 모든 사용자들에게 메시지 발송
+        //클라이언트에서 메시지 데이터를 json포맷으로 전송
+        socket.on("channelMsg",async(msgData)=>{
+            //io.to('채널명').emit()는 현재채널에 메시지를 보낸 당사자(나를) 포함한 현재 채널 사용자에게
+            io.to(msgData.channel).emit("receiveChannel",`${msgData.nickName} : ${msgData.message}`);
+        });
+
+        socket.on('exit',async(channel,nickName)=>{
+            socket.to(channel).emit("exitOk",`${nickName}님이 퇴장했습니다.`);
+
+            socket.leave(channel);
+
+            socket.emit("exitOk", `채팅방을 퇴장했습니다.`);
+        });
+    });
+}
